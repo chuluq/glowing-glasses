dae69d340a09491f262af21e1991952f6264f390
chuluq <chuluq@arvis.id>
2023-12-30 11:18:06 +0700

    feat: add link socials

--- src/config/site.ts
+++ src/config/site.ts
@@ -5,8 +5,8 @@ export const siteConfig: SiteConfig = {
   description:
     "Discover the virtuosity of a web developer passionate about turning ideas into interactive digital masterpieces. Navigate the realms of code with finesse.",
   socials: {
-    github: "https://github.com",
-    instagram: "https://instagram.com",
+    github: "https://github.com/chuluq",
+    instagram: "https://www.instagram.com/choirulchuluq",
     // twitter: "https://twitter.com",
     // whatsapp: "https:///whatsapp.com",
   },
